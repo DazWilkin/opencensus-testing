07ca7172c5f80e95a9b474f75f332d876dfe3387
Daz Wilkin <dazwilkin@google.com>
2018-12-28 15:51:23 -0800

    Added Options for Datadog

--- datadog/datadog.go
+++ datadog/datadog.go
@@ -3,6 +3,7 @@ package datadog
 import (
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/dazwilkin/opencensus/stats/view"
@@ -14,13 +15,15 @@ var (
 )
 
 type Importer struct {
-	name string
+	name    string
+	options Options
 }
 
 // NewImporter creates a new importer using the Options provided
 func NewImporter(o Options) (*Importer, error) {
 	return &Importer{
-		name: "datadog",
+		name:    "datadog",
+		options: o,
 	}, nil
 }
 
@@ -29,14 +32,34 @@ func (i *Importer) Name() string {
 	return i.name
 }
 
+// Stop closes the connection to the Datadog service
+func (i *Importer) Stop() {
+	log.Println("[Stop] Does nothing")
+}
+
 // Value returns the Importer's value for the View, with the label values and the time specified
 func (i *Importer) Value(v *view.View, labelValues []string, t time.Time) (float64, error) {
 
 	from := t.Add(time.Minute * -1)
 
-	query := NewQuery(v.Name)
+	//TODO(dazwilkin) Datadog appears to append label names to the metric name, try it out
+	query := NewQuery(func(v *view.View) string {
+		// Name
+		name := v.Name
+		// Prefix Namespace, if one exists
+		if i.options.Namespace != "" {
+			name = i.options.Namespace + "." + name
+		}
+		// Append label names
+		if len(v.LabelNames) > 0 {
+			name = name + "_" + strings.Join(v.LabelNames, "_")
+		}
+		return name
+	}(v))
+
 	host, _ := os.Hostname()
 	query.AddHostname(host)
+
 	for i, labelName := range v.LabelNames {
 		query.AddTagValue(labelName, labelValues[i])
 	}
@@ -47,36 +70,32 @@ func (i *Importer) Value(v *view.View, labelValues []string, t time.Time) (float
 		log.Fatal(err)
 	}
 
-	// for _, s := range ss {
-	// 	log.Printf("Metric: %v", *s.Metric)
-	// 	for _, p := range s.Points {
-	// 		log.Printf("[%v] %v", time.Unix(0, int64(*p[0])*int64(time.Millisecond)), *p[1])
-	// 	}
-	// }
-
+	// If there is a time-series, grab the most recent one
 	if len(ss) >= 1 {
 		s := ss[0]
 		log.Printf("Metric: %v", *s.Metric)
+		// If the time-series contains any data points, grab the most recent one
 		if len(s.Points) >= 1 {
 			p := s.Points[0]
+			// *p[0] == Unix epoch timestamp in ms
+			// *p[1] == data
 			log.Printf("[%v] %v", time.Unix(0, int64(*p[0])*int64(time.Millisecond)), *p[1])
 			return *p[1], nil
 		}
 		return 0.0, nil
 	}
-
 	return 0.0, nil
 }
 
 // Options represents the configuration of an OpenCensus Importer
 type Options struct {
-	MetricPrefix string
+	Namespace string
 }
 
 //TODO(dazwilkin) Instead of package init should this by a type func or helper?
 func init() {
-	API := os.Getenv("dd.API")
-	App := os.Getenv("dd.App")
+	API := os.Getenv("DD_API")
+	App := os.Getenv("DD_APP")
 	if API == "" || App == "" {
 		log.Fatal("Expect Datadog API and App values")
 	}
